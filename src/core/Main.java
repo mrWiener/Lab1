2726a67698aa249c1fc54bae7dfb8d59e9035747
Lucas Wiener <lucas@wiener.se>
2012-09-10 14:12:48 +0200

    JAVA :D :D

---
+++ src/core/Main.java
@@ -0,0 +1,19 @@
+package core;
+
+public class Main {
+	public static void main(String[] args) {
+		if (args.length == 1) {
+			String word = args[0];
+			
+			System.out.println("Word to analyze: " + word);
+		}
+		else if (args.length == 2) {
+			String file = args[1];
+			
+			System.out.println(file);
+		}
+		else {
+			System.out.println("Specify arguments.");
+		}
+	}
+}
