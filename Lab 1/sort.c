4b9e2ab2df4b43d0b2e478aa10fed75cbb94421c
Lucas Wiener <lucas@wiener.se>
2012-09-10 11:03:58 +0200

    Added sort files

---
+++ Lab 1/sort.c
@@ -0,0 +1,9 @@
+//
+//  sort.c
+//  Lab 1
+//
+//  Created by Lucas Wiener on 9/10/12.
+//  Copyright (c) 2012 skola. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ Lab 1/sort.h
@@ -0,0 +1,14 @@
+//
+//  sort.h
+//  Lab 1
+//
+//  Created by Lucas Wiener on 9/10/12.
+//  Copyright (c) 2012 skola. All rights reserved.
+//
+
+#ifndef Lab_1_sort_h
+#define Lab_1_sort_h
+
+
+
+#endif
